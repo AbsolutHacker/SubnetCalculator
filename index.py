5583591722b9030fcc7f66ecf2d9eaf6d967eba7
rune.wethmar <rune.wethmar@nx-technologies.com>
2024-12-12 12:45:32 +0100

    commentet all requirements in index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+# Anforderungskriterien:
+# Die maximale Anzahl der zur Verfügung stehenden IP-Adressen und die davon abhängigen Host-Adressen müssen
+# dargestellt werden
+# Die Broadcastadresse muss ausgegeben werden
+# Die Netzwerkadresse muss ausgegeben werden
+# Die Netzmaske muss in der CIDR-Notation ausgegeben werden
+# die Anzahl der Netzwerk- und Host-Bits muss ausgegeben werden.
+# die Umrechnung erfolgt grafisch nachvollziehbar und nur mit Hilfe von Grundrechenarten (ggf. modulo und Potenzrechnung).
+# Es soll keine Makro-Programmierung unter Excel stattfinden oder unter Python keine (Fremd-) Bibliotheken eingesetzt werden.
+# Netzwerk- und Hostanteil müssen durch farbige Hinterlegung der Bits grafisch deutlich werden
