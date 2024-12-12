6ace73b7cd9a4cc43d1e1ec4a1b6006fc201c822
Nick Seufert <nicolas.seufert@gmail.com>
2024-12-12 13:27:34 +0100

    Erstes MVP

--- index.py
+++ index.py
@@ -11,3 +11,35 @@
 
 inputIP = input('Bitte geben sie die IP Adresse ein ')
 inputMask = input('bitte geben sie die Subnetzmaske ein ')
+
+def dotted_decimal_to_bitfield(string: str):
+    result = [0, 0, 0, 0]
+    octets = [ int(octet) for octet in string.split('.') ]
+    i = 0
+    for octet in octets:
+        if octet > 255 or octet < 0:
+            raise ValueError('Value out of range: ' + str(octet))
+        result[i] = octet
+        i += 1
+    return (result[0] << 24) + (result[1] << 16) + (result[2] << 8) + result[3]
+
+def bitfield_to_dotted_decimal(bitfield: int):
+    octets = [ int((bitfield >> 24) & 0xFF), int((bitfield >> 16) & 0xFF), int((bitfield >> 8) & 0xFF), int(bitfield & 0xFF) ]
+    return '.'.join(map(str, octets))
+
+addr = dotted_decimal_to_bitfield(inputIP)
+
+if '.' in inputMask:
+    mask = dotted_decimal_to_bitfield(inputMask)
+else:
+    # TODO: das noch richtig machen
+    mask = -1 << (32 - int(inputMask))
+
+nwad = addr & mask
+bcad = nwad | (~mask)
+
+print(bitfield_to_dotted_decimal(addr))
+print(bitfield_to_dotted_decimal(mask))
+print(bitfield_to_dotted_decimal(nwad))
+print(bitfield_to_dotted_decimal(bcad))
+
